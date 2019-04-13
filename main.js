d0125cf42a693c492d04c95b9d70aee7b13f8765
Oliver Smith <oli-smith@live.co.uk>
2019-04-13 14:51:01 +0100

    adding basic JS for dropdown burger menu on mobile and hide-on-scroll for the nav bar

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
 // -------------------------------------Burger Menu script
 const burgerButton = document.getElementById("burgerButton");
-const burger = document.getElementById("burgerMenu");
+const burgerMenu = document.getElementById("burgerMenu");
+const burgerIcon = document.getElementById("burgerIcon");
 
 const hidden = "hiddenMenu";
 const down = "menuSlideDown";
-const up = "menuSlideUp;";
+const up = "menuSlideUp";
 const unhidden = "unhiddenMenu";
+const startways = "menuOrigin";
+const sideways = "menuOpen";
+const rightways = "menuClosed";
 
-burgerButton.onclick = () => {
-  if (burger.classList.contains(hidden)) {
-    burger.classList.add(down);
-    burger.classList.remove(hidden);
-    setTimeout(() => burger.classList.add(unhidden), 750);
-  } else if (burger.classList.contains(down)) {
-    burger.classList.add(up);
-    setTimeout(() => burger.classList.remove(unhidden), 0001);
-    burger.classList.remove(down);
-  } else if (burger.classList.contains(up)) {
-    burger.classList.add(down);
-    burger.classList.remove(up);
-    setTimeout(() => burger.classList.add(unhidden), 750);
+const burgerClick = (burgerButton.onclick = () => {
+  if (burgerMenu.classList.contains(hidden)) {
+    burgerMenu.classList.add(down);
+    burgerMenu.classList.remove(hidden);
+    burgerMenu.classList.add(up);
+    burgerMenu.classList.remove(up);
+    setTimeout(() => burgerMenu.classList.add(unhidden), 500);
+    burgerIcon.classList.remove(startways);
+    burgerIcon.classList.remove(rightways);
+    burgerIcon.classList.add(sideways);
+  } else if (burgerMenu.classList.contains(unhidden)) {
+    burgerMenu.classList.add(up);
+    burgerMenu.classList.remove(down);
+    burgerMenu.classList.add(hidden);
+    setTimeout(() => burgerMenu.classList.remove(unhidden), 500);
+    burgerIcon.classList.remove(sideways);
+    burgerIcon.classList.add(rightways);
+    burgerIcon.classList.add(startways);
   }
+});
+
+// -----------------------------------Hide/Unhide menu onScroll
+
+const navBar = document.getElementById("navBar");
+var previousPosition = window.pageYOffset;
+
+window.onscroll = () => {
+  var currentPosition = window.pageYOffset;
+
+  if (previousPosition > currentPosition) {
+    navBar.style.top = "8px";
+  } else {
+    navBar.style.top = "-50vh";
+  }
+  previousPosition = currentPosition;
 };
-// ---------------------------------------------------------
